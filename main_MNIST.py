b0b244d5e1452b5584eddb93bdfcd482682e9a16
Elias Wendt <git@eliaswendt.de>
2023-04-23 17:45:09 +0200

    Remove thread and rename explainer-config arg

--- main_MNIST.py
+++ main_MNIST.py
@@ -7,7 +7,7 @@ parser.add_argument('-lr', '--learning-rate', type=float, default=1e-3)
 parser.add_argument('-wd', '--weight-decay', type=float, default=1e-4)
 parser.add_argument('-e', '--epochs', type=int, default=50)
 parser.add_argument('-sb', '--save-best-epoch', action='store_true')
-parser.add_argument('-t', '--num-threads', type=int, default=5)
+#parser.add_argument('-t', '--num-threads', type=int, default=5)
 parser.add_argument('-ce', '--generate-counterexamples', action='store_true')
 parser.add_argument('-rt', '--reduced-train-set', type=int, help="if set will shrink train set to x * batch_size")
 
@@ -20,7 +20,7 @@ parser.add_argument('--rbr', type=int)
 
 parser.add_argument('-r', '--runs', type=int, default=[1, 2, 3, 4, 5], choices=[1, 2, 3, 4, 5], nargs='+', help='Which runs to perform (each run has a different seed)?')
 
-parser.add_argument('--explainer-config', default='GradCAM IG LIME Saliency IxG DeepLift LRP GBP IntGrad', type=str,
+parser.add_argument('--explainer', default='GradCAM IG LIME Saliency IxG DeepLift LRP GBP IntGrad', type=str,
                     choices=['GradCAM', 'IG', 'LIME', 'Saliency', \
                              'IxG', 'DeepLift', 'LRP', 'GBP', 'IntGrad'], nargs='+',
                     help='Which explainers to use?')
@@ -65,11 +65,12 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 logging.info(f"DEVICE={DEVICE}")
 
 torch.set_printoptions(linewidth=150)
-torch.set_num_threads(args.num_threads)
+#torch.set_num_threads(args.num_threads)
 
 # collect trained learners in list (gets iterated during eval)
 trained_learners = []
 
+# inform about run progress (instead of epochs)
 rtpt = RTPT(
     name_initials='EW',
     experiment_name='Learner', 
@@ -163,7 +164,7 @@ for i, learner in enumerate(trained_learners):
         #generate_counterexamples=args.generate_counterexamples, # TODO: discuss whether to allow CEs here
     )
 
-    if 'GradCAM' in args.explainer_config:
+    if 'GradCAM' in args.explainer:
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_grad/', exist_ok=True)
         explainer.explain_with_captum('grad_cam', learner.model, test_loader, range(len(test_loader)), \
                                       next_to_each_other=False,
@@ -175,7 +176,7 @@ for i, learner in enumerate(trained_learners):
                                                     name=f'{loss_config_string}-grad', wr_name=learner.modelname + "--grad", \
                                                     threshold=thresh, flags=False, device=DEVICE))
 
-    if 'Saliency' in args.explainer_config:
+    if 'Saliency' in args.explainer:
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_saliency/', exist_ok=True)
         explainer.explain_with_captum('saliency', learner.model, test_loader, range(len(test_loader)), \
                                       next_to_each_other=False,
@@ -187,7 +188,7 @@ for i, learner in enumerate(trained_learners):
                                                     name=f'{loss_config_string}-saliency', wr_name=learner.modelname + "--saliency", \
                                                     threshold=thresh, flags=False, device=DEVICE))
 
-    if 'IxG' in args.explainer_config:
+    if 'IxG' in args.explainer:
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_input_x_gradient/', exist_ok=True)
         explainer.explain_with_captum('input_x_gradient', learner.model, test_loader, range(len(test_loader)), \
                                       next_to_each_other=False,
@@ -201,7 +202,7 @@ for i, learner in enumerate(trained_learners):
                                                     wr_name=learner.modelname + "--input_x_gradient", \
                                                     threshold=thresh, flags=False, device=DEVICE))
 
-    if 'DeepLift' in args.explainer_config:
+    if 'DeepLift' in args.explainer:
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_deep_lift/', exist_ok=True)
         explainer.explain_with_captum('deep_lift', learner.model, test_loader, range(len(test_loader)), \
                                       next_to_each_other=False,
@@ -213,7 +214,7 @@ for i, learner in enumerate(trained_learners):
                                                     name=f'{loss_config_string}-deep_lift', wr_name=learner.modelname + "--deep_lift", \
                                                     threshold=thresh, flags=False, device=DEVICE))
 
-    if 'LRP' in args.explainer_config:
+    if 'LRP' in args.explainer:
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_lrp/', exist_ok=True)
         explainer.explain_with_captum('lrp', learner.model, test_loader, range(len(test_loader)), \
                                       next_to_each_other=False,
@@ -225,7 +226,7 @@ for i, learner in enumerate(trained_learners):
                                                     name=f'{loss_config_string}-lrp', wr_name=learner.modelname + "--lrp", \
                                                     threshold=thresh, flags=False, device=DEVICE))
 
-    if 'GBP' in args.explainer_config:
+    if 'GBP' in args.explainer:
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_guided_backprop/', exist_ok=True)
         explainer.explain_with_captum('guided_backprop', learner.model, test_loader, range(len(test_loader)), \
                                       next_to_each_other=False,
@@ -240,7 +241,7 @@ for i, learner in enumerate(trained_learners):
                                                     threshold=thresh, flags=False, device=DEVICE))
 
     #not implemented
-    if 'IntGrad' in args.explainer_config:
+    if 'IntGrad' in args.explainer:
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_integrated_gradients/', exist_ok=True)
         explainer.explain_with_captum('integrated_gradients', learner.model, test_loader, range(len(test_loader)), \
                                       next_to_each_other=False,
@@ -254,7 +255,7 @@ for i, learner in enumerate(trained_learners):
                                                     wr_name=learner.modelname + "--integrated_gradients", \
                                                     threshold=thresh, flags=False, device=DEVICE))
 
-    if 'IG' in args.explainer_config:
+    if 'IG' in args.explainer:
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_ig/', exist_ok=True)
         explainer.explain_with_ig(learner.model, test_loader, range(len(test_loader)), \
                                   next_to_each_other=False,
@@ -266,7 +267,7 @@ for i, learner in enumerate(trained_learners):
                                                     name=f'{loss_config_string}-ig', wr_name=learner.modelname + "--ig", \
                                                     threshold=thresh, flags=False, device=DEVICE))
 
-    if 'LIME' in args.explainer_config:
+    if 'LIME' in args.explainer:
         print("lime start")
         os.makedirs(f'output_images/{args.dataset}-expl/{loss_config_string}_lime/', exist_ok=True)
         explainer.explain_with_lime(learner.model, test_loader, range(len(test_loader)), \
@@ -287,23 +288,23 @@ for i, learner in enumerate(trained_learners):
     # write results to file
     f = open(f"./output_wr_metric/{args.dataset}-{loss_config_string}.txt", "w")
     
-    if 'IG' in args.explainer_config:
+    if 'IG' in args.explainer:
         f.write(f'IG P: mean:{np.mean(avg_ig)}, std:{np.std(avg_ig)}\n')
-    if 'GradCAM' in args.explainer_config:
+    if 'GradCAM' in args.explainer:
         f.write(f'Grad P: mean:{np.mean(avg_gradcam)}, std:{np.std(avg_gradcam)}\n')
-    if 'LIME' in args.explainer_config:
+    if 'LIME' in args.explainer:
         f.write(f'LIME P: mean:{np.mean(avg_lime)}, std:{np.std(avg_lime)}\n')
-    if 'Saliency' in args.explainer_config:
+    if 'Saliency' in args.explainer:
         f.write(f'Saliency P: mean:{np.mean(avg_saliency)}, std:{np.std(avg_saliency)}\n')
-    if 'IxG' in args.explainer_config:
+    if 'IxG' in args.explainer:
         f.write(f'IxG P: mean:{np.mean(avg_ixg)}, std:{np.std(avg_ixg)}\n')
-    if 'DeepLift' in args.explainer_config:
+    if 'DeepLift' in args.explainer:
         f.write(f'DL P: mean:{np.mean(avg_deeplift)}, std:{np.std(avg_deeplift)}\n')
-    if 'LRP' in args.explainer_config:
+    if 'LRP' in args.explainer:
         f.write(f'LRP P: mean:{np.mean(avg_lrp)}, std:{np.std(avg_lrp)}\n')
-    if 'GBP' in args.explainer_config:
+    if 'GBP' in args.explainer:
         f.write(f'GBP P: mean:{np.mean(avg_gbp)}, std:{np.std(avg_gbp)}\n')
-    if 'IntGrad' in args.explainer_config:
+    if 'IntGrad' in args.explainer:
         f.write(f'IntGrad P: mean:{np.mean(avg9)}, std:{np.std(avg9)}\n')
 
     f.close()
