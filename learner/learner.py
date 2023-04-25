c68119c39097553f29b08ac0a5c8e679ad537aa7
Elias Wendt <git@eliaswendt.de>
2023-04-25 09:38:11 +0200

    Introduce reg-rate evaluation, improve train status readability, and improve checkpointing

--- learner/learner.py
+++ learner/learner.py
@@ -9,7 +9,7 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from tqdm import tqdm
 from xil_methods.xil_loss import RRRGradCamLoss, RRRLoss, CDEPLoss, HINTLoss, HINTLoss_IG, RBRLoss
 from datetime import datetime
-
+from collections import defaultdict
 
 class Learner:
     """
@@ -36,6 +36,58 @@ class Learner:
         # model automatically tries to load checkpoint from previous run
         self.load_from_checkpoint()
 
+
+    def evaluate_regularization_rates(self, train_loader):
+        """
+        Will collect losses of all loss-functions on `train_loader` and return rates to regulate all losses to 1
+        """
+
+        loss_sum_ra = 0
+
+        loss_function_rr_rrr = RRRLoss(regularizer_rate=1)
+        loss_sum_rr_rrr = 0
+        
+        loss_function_rrr_gc = RRRGradCamLoss(regularizer_rate=1)
+        loss_sum_rrr_gc = 0
+        
+        loss_function_rr_cdep = CDEPLoss(regularizer_rate=1)
+        loss_sum_rr_cdep = 0
+        
+        loss_function_rr_hint = HINTLoss(regularizer_rate=1)
+        loss_sum_rr_hint = 0
+        
+        loss_function_rr_hint_ig = HINTLoss_IG(regularizer_rate=1)
+        loss_sum_rr_hint_ig = 0
+        
+        loss_function_rr_rbr = RBRLoss(regularizer_rate=1)
+        loss_sum_rr_rbr = 0
+
+        # iterate all batches of train
+        for X, y, E_pnlt, E_rwrd, ce_mask in tqdm(train_loader, unit="batch"):
+            X, y, E_pnlt, E_rwrd = X[~ce_mask], y[~ce_mask], E_pnlt[~ce_mask], E_rwrd[~ce_mask]
+
+            X.requires_grad_()
+
+            if len(X) > 0:  # required as rrr doesn't work on zero-sized tensors
+                y_hat = self.model(X)
+                loss_ra = self.loss_function_right_answer(y_hat, y)
+                loss_sum_ra += loss_ra
+
+                loss_sum_rr_rrr += loss_function_rr_rrr.forward(X, E_rwrd, y_hat)  # todo check changes!
+                loss_sum_rrr_gc += loss_function_rrr_gc.forward(self.model, X, y, E_rwrd, y_hat, self.device)
+                loss_sum_rr_cdep += loss_function_rr_cdep.forward(self.model, X, y, E_rwrd, self.device)
+                loss_sum_rr_hint += loss_function_rr_hint.forward(self.model, X, y, E_rwrd, self.device)  # todo check changes!
+                loss_sum_rr_hint_ig += loss_function_rr_hint_ig.forward(self.model, X, E_rwrd, y_hat, self.device)  # todo check changes!
+                loss_sum_rr_rbr += loss_function_rr_rbr.forward(self.model, X, y, loss_ra, E_rwrd, y_hat)
+
+        logging.info(f'calculated regularization_rates: rrr={loss_sum_ra/loss_sum_rr_rrr},rrr_gc={loss_sum_ra/loss_sum_rrr_gc},cdep={loss_sum_ra/loss_sum_rr_cdep},hint={loss_sum_ra/loss_sum_rr_hint},hint_ig={loss_sum_ra/loss_sum_rr_hint_ig},rbr={loss_sum_ra/loss_sum_rr_rbr}')
+        return \
+            (loss_sum_ra/loss_sum_rr_rrr).item(),\
+            (loss_sum_ra/loss_sum_rrr_gc).item(),\
+            (loss_sum_ra/loss_sum_rr_cdep).item(),\
+            (loss_sum_ra/loss_sum_rr_hint).item(),\
+            (loss_sum_ra/loss_sum_rr_hint_ig).item(),\
+            (loss_sum_ra/loss_sum_rr_rbr).item()
 
     def score(self, dataloader, criterion, verbose=False):
         """Returns the acc and loss on the specified dataloader."""
@@ -109,173 +161,145 @@ class Learner:
 
         print("Start training...")
 
-        best_epoch_loss = 100000
+        best_epoch_loss = 10000000
         elapsed_time = 0
 
         for epoch in range(self.n_trained_epochs+1, epochs+1):
+            # collecting losses of epoch
+            epoch_losses = defaultdict(lambda: torch.tensor(0., device=self.device))
+
+
             self.model.train()
             len_dataset = len(train_loader.dataset)
 
-            # sums of losses within epoch
-            epoch_loss_right_answer_ce = 0.
-            epoch_loss_right_answer = 0.
-            epoch_loss_right_reason = 0.
-            epoch_loss_rrr = 0.
-            epoch_loss_rrr_gc = 0.
-            epoch_loss_cdep = 0.
-            epoch_loss_hint = 0.
-            epoch_loss_hint_ig = 0.
-            epoch_loss_rbr = 0.
-
+            # counts the number of correctly classified instances during current epoch
             epoch_correct = 0
 
             epoch_start_time = time.time()
 
-            for X, y, E_pnlt, E_rwrd, ce_mask in tqdm(train_loader, unit='batch'):
+            # iterate batches
+            for X, y, E_pnlt, E_rwrd, ce_mask in train_loader:
+                logging.debug(f"batch consists of {len(X[~ce_mask])} examples and {len(X[ce_mask])} counterexamples")
+
+                # collecting losses of batch
+                batch_losses = defaultdict(lambda: torch.tensor(0.))
 
                 self.optimizer.zero_grad()
                 X.requires_grad_()
-
-                logging.info(f"batch consists of {len(X[~ce_mask])} examples and {len(X[ce_mask])} counterexamples")
-
-                # initialize zero-loss tensors (as they may be unbound)
-                batch_loss_right_answer_ce = torch.tensor(
-                    0., device=self.device)
-                batch_loss_right_answer = torch.tensor(0., device=self.device)
-                batch_loss_right_reason = torch.tensor(0., device=self.device)
 
                 # compute right-answer loss on CEs
                 X_ce, y_ce, _, _ = X[ce_mask], y[ce_mask], E_pnlt[ce_mask], E_rwrd[ce_mask]
                 if len(X_ce) > 0:
                     y_hat_ce = self.model(X_ce)
-                    epoch_correct += (y_hat_ce.argmax(1) ==
-                                      y_ce).type(torch.float).sum().item()
-                    batch_loss_right_answer_ce += self.loss_function_right_answer(
-                        y_hat_ce, y_ce)
-                    epoch_loss_right_answer_ce += batch_loss_right_answer_ce
-                    logging.debug(
-                        f"loss_right_answer_ce={batch_loss_right_answer_ce}")
+                    epoch_correct += (y_hat_ce.argmax(1) == y_ce).type(torch.float).sum().item()
+                    loss = self.loss_function_right_answer(y_hat_ce, y_ce)
+                    batch_losses['ra_ce'] += loss
+                    epoch_losses['ra_ce'] += loss
 
                 # compute right-answer AND right-reason loss on non-CEs
                 X, y, E_pnlt, E_rwrd = X[~ce_mask], y[~ce_mask], E_pnlt[~ce_mask], E_rwrd[~ce_mask]
                 if len(X) > 0:  # required as rrr doesn't work on zero-sized tensors
                     y_hat = self.model(X)
-                    epoch_correct += (y_hat.argmax(1) ==
-                                      y).type(torch.float).sum().item()
-                    batch_loss_right_answer = self.loss_function_right_answer(y_hat, y)
-                    epoch_loss_right_answer += batch_loss_right_answer
-                    logging.debug(
-                        f"loss_right_answer={batch_loss_right_answer}")
+                    epoch_correct += (y_hat.argmax(1) == y).type(torch.float).sum().item()
+                    loss = self.loss_function_right_answer(y_hat, y)
+                    batch_losses['ra_non_ce'] += loss
+                    epoch_losses['ra_non_ce'] += loss
 
                     ###################
                     # MultiLoss START #
                     ###################
 
                     if loss_function_rrr:
-                        batch_loss_rrr = loss_function_rrr.forward(
-                            X, E_rwrd, y_hat)  # todo check implementation changes!
-                        # print(f"loss_rrr={loss_rrr}")
-                        batch_loss_right_reason += batch_loss_rrr
-                        epoch_loss_rrr += batch_loss_rrr
+                        loss = loss_function_rrr.forward(X, E_rwrd, y_hat)  # todo check changes!
+                        
+                        batch_losses['rr_rrr'] += loss
+                        epoch_losses['rr_rrr'] += loss
+                        batch_losses['rr'] += loss
+                        epoch_losses['rr'] += loss
 
                     if loss_function_rrr_gc:
-                        batch_loss_rrr_gc = loss_function_rrr_gc.forward(
-                            self.model, X, y, E_rwrd, y_hat, self.device)  # changes verified
-                        # print(f"loss_rrr_gc={loss_rrr_gc}")
-                        batch_loss_right_reason += batch_loss_rrr_gc
-                        epoch_loss_rrr_gc += batch_loss_rrr_gc
+                        loss = loss_function_rrr_gc.forward(self.model, X, y, E_rwrd, y_hat, self.device)
+                        
+                        batch_losses['rr_rrr_gc'] += loss
+                        epoch_losses['rr_rrr_gc'] += loss
+                        batch_losses['rr'] += loss
+                        epoch_losses['rr'] += loss
 
                     if loss_function_cdep:
-                        batch_loss_cdep = loss_function_cdep.forward(
-                            self.model, X, y, E_rwrd, self.device)  # changes verified
-                        # print(f"loss_cdep={loss_cdep}")
-                        batch_loss_right_reason += batch_loss_cdep
-                        epoch_loss_cdep += batch_loss_cdep
+                        loss = loss_function_cdep.forward(self.model, X, y, E_rwrd, self.device)
+
+                        batch_losses['rr_cdep'] += loss
+                        epoch_losses['rr_cdep'] += loss
+                        batch_losses['rr'] += loss
+                        epoch_losses['rr'] += loss
 
                     if loss_function_hint:
-                        batch_loss_hint = loss_function_hint.forward(
-                            self.model, X, y, E_rwrd, self.device)  # todo check implementation changes!
-                        # print(f"loss_hint={loss_hint}")
-                        batch_loss_right_reason += batch_loss_hint
-                        epoch_loss_hint += batch_loss_hint
+                        loss = loss_function_hint.forward(self.model, X, y, E_rwrd, self.device)  # todo check changes!
+
+                        batch_losses['rr_hint'] += loss
+                        epoch_losses['rr_hint'] += loss
+                        batch_losses['rr'] += loss
+                        epoch_losses['rr'] += loss
 
                     if loss_function_hint_ig:
-                        batch_loss_hint_ig = loss_function_hint_ig.forward(
-                            self.model, X, E_rwrd, y_hat, self.device)  # todo check implementation changes!
-                        # print(f"loss_hint={batch_loss_hint_ig}")
-                        batch_loss_right_reason += batch_loss_hint_ig
-                        epoch_loss_hint_ig += batch_loss_hint_ig
+                        loss = loss_function_hint_ig.forward(self.model, X, E_rwrd, y_hat, self.device)  # todo check changes!
+
+                        batch_losses['rr_hint_ig'] += loss
+                        epoch_losses['rr_hint_ig'] += loss
+                        batch_losses['rr'] += loss
+                        epoch_losses['rr'] += loss
 
                     if loss_function_rbr:
-                        batch_loss_rbr = loss_function_rbr.forward(
-                            self.model, X, y, batch_loss_right_answer, E_rwrd, y_hat)  # changes verified
-                        # print(f"loss_rbr={loss_rbr}")
-                        batch_loss_right_reason += batch_loss_rbr
-                        epoch_loss_rbr += batch_loss_rbr
+                        loss = loss_function_rbr.forward(self.model, X, y, batch_losses['ra_non_ce'], E_rwrd, y_hat)
 
-                    epoch_loss_right_reason += batch_loss_right_reason
-                    logging.debug(
-                        f"loss_right_reason={batch_loss_right_reason}")
+                        batch_losses['rr_rbr'] += loss
+                        epoch_losses['rr_rbr'] += loss
+                        batch_losses['rr'] += loss
+                        epoch_losses['rr'] += loss
 
                     #################
                     # MultiLoss END #
                     #################
 
-                (batch_loss_right_answer_ce + batch_loss_right_answer +
-                    batch_loss_right_reason).backward()
+                (batch_losses['ra_ce'] + batch_losses['ra_non_ce'] + batch_losses['rr']).backward()
                 self.optimizer.step()
 
-            logging.info(f"epoch losses: ra={epoch_loss_right_answer}, ra_ce={epoch_loss_right_answer_ce}, rr_hint={epoch_loss_hint}, rr_hint_ig={epoch_loss_hint_ig}, rr_rrr={epoch_loss_rrr}, rr_rrr_gc={epoch_loss_rrr_gc}, rr_cdep={epoch_loss_cdep}, rr_rbr={epoch_loss_rbr}")
+            epoch_loss = epoch_losses['ra_ce'] + epoch_losses['ra_non_ce'] + epoch_losses['rr']
 
-            epoch_loss_right_answer /= len_dataset
-            epoch_loss_right_reason /= len_dataset
             epoch_correct /= len_dataset
             train_acc = 100. * epoch_correct
-            epoch_loss = epoch_loss_right_answer_ce + \
-                epoch_loss_right_answer + epoch_loss_right_reason
 
             elapsed_time_cur = time.time() - epoch_start_time
             elapsed_time += elapsed_time_cur
 
             tensorboard_writer.add_scalar('Loss/train', epoch_loss, epoch)
 
-            tensorboard_writer.add_scalar(
-                'Loss/ra', epoch_loss_right_answer, epoch)
-            tensorboard_writer.add_scalar(
-                'Loss/ra_ce', epoch_loss_right_answer_ce, epoch)
-            tensorboard_writer.add_scalar(
-                'Loss/rr', epoch_loss_right_reason, epoch)
+            tensorboard_writer.add_scalar('Loss/right_answer_non_counterexamples', epoch_losses['ra_non_ce'], epoch)
+            tensorboard_writer.add_scalar('Loss/right_answer_counterexamples', epoch_losses['ra_ce'], epoch)
+            tensorboard_writer.add_scalar('Loss/right_reason_total', epoch_losses['rr'], epoch)
 
-            tensorboard_writer.add_scalar(
-                'Loss/rrr', epoch_loss_rrr, epoch)
-            tensorboard_writer.add_scalar(
-                'Loss/rrr_gc', epoch_loss_rrr_gc, epoch)
-            tensorboard_writer.add_scalar(
-                'Loss/cdep', epoch_loss_cdep, epoch)
-            tensorboard_writer.add_scalar(
-                'Loss/hint', epoch_loss_hint, epoch)
-            tensorboard_writer.add_scalar(
-                'Loss/hint_ig', epoch_loss_hint_ig, epoch)
-            tensorboard_writer.add_scalar(
-                'Loss/rbr', epoch_loss_rbr, epoch)
+            tensorboard_writer.add_scalar('Loss/right_reason_rrr', epoch_losses['rr_rrr'], epoch)
+            tensorboard_writer.add_scalar('Loss/right_reason_rrr_gc', epoch_losses['rr_rrr_gc'], epoch)
+            tensorboard_writer.add_scalar('Loss/right_reason_cdep', epoch_losses['rr_cdep'], epoch)
+            tensorboard_writer.add_scalar('Loss/right_reason_hint', epoch_losses['rr_hint'], epoch)
+            tensorboard_writer.add_scalar('Loss/right_reason_hint_ig', epoch_losses['rr_hint_ig'], epoch)
+            tensorboard_writer.add_scalar('Loss/right_reason_rbr', epoch_losses['rr_rbr'], epoch)
 
             tensorboard_writer.add_scalar('Acc/train', train_acc, epoch)
-            tensorboard_writer.add_scalar(
-                'Time/train', elapsed_time_cur, epoch)
+            tensorboard_writer.add_scalar('Time/train', elapsed_time_cur, epoch)
 
-            val_acc, val_loss = self.score(
-                test_loader, self.loss_function_right_answer)
+            test_acc, test_loss = self.score(test_loader, self.loss_function_right_answer)
 
-            tensorboard_writer.add_scalar('Loss/test', val_loss, epoch)
-            tensorboard_writer.add_scalar('Acc/test', val_acc, epoch)
+            tensorboard_writer.add_scalar('Loss/test', test_loss, epoch)
+            tensorboard_writer.add_scalar('Acc/test', test_acc, epoch)
             tensorboard_writer.flush()
 
-            logging.info(
-                f"Epoch {epoch}| accuracy: {(train_acc):>0.1f}%, loss: {epoch_loss:>8f} | Test Error: Acc: {val_acc:>0.1f}%, Avg loss: {val_loss:>8f}")
+            losses_summary = ', '.join(map(lambda x: f'{x[0]}={x[1].item():,.3f}', epoch_losses.items()))
+            print(f'E={epoch:0>2} | train_acc: {(train_acc):>0.1f}%, train_loss: {epoch_loss:>8f} | test_acc: {test_acc:>0.1f}%, test_loss: {test_loss:>8f} | {losses_summary}')
 
             # write in logfile -> we need the logfile to see plots in Jupyter notebooks
             log_writer.write(
-                f"{epoch},{(train_acc):>0.1f},{epoch_loss:>8f},{epoch_loss_right_answer:>8f},{epoch_loss_right_reason:>8f},{(val_acc):>0.1f},{val_loss:>8f},{elapsed_time_cur:>0.4f}\n")
+                f"{epoch},{(train_acc):>0.1f},{epoch_loss:>8f},{epoch_losses['ra_non_ce']:>8f},{epoch_losses['rr']:>8f},{test_acc:>0.1f},{test_loss:>8f},{elapsed_time_cur:>0.4f}\n")
             log_writer.flush()
 
             # # log to terminal on switch
@@ -302,17 +326,19 @@ class Learner:
         Save the model dict to disk.
         """
 
+        modelname = self.modelname + '_best' if best else self.modelname
+
         checkpoint = {
             'weights': self.model.state_dict(),
             'optimizer_dict': self.optimizer.state_dict(),
-            'modelname':  self.modelname + "-bestOnTrain" if best else self.modelname,
+            'modelname': modelname,
             'n_trained_epochs': n_trained_epochs,
             'rng_state': torch.get_rng_state()
         }
 
-        path = f'learner/model_store/{self.modelname}.pt'
+        path = f'learner/model_store/{modelname}.pt'
         torch.save(checkpoint, path)
-        logging.info(f'model saved to "{path}"')
+        logging.debug(f'model saved to "{path}"')
 
 
     def load_from_checkpoint(self):
@@ -327,9 +353,14 @@ class Learner:
             self.model.load_state_dict(checkpoint['weights'])
             self.optimizer.load_state_dict(checkpoint['optimizer_dict'])
             torch.set_rng_state(checkpoint['rng_state'].type(torch.ByteTensor))
-            self.n_trained_epochs = checkpoint['n_trained_epochs']
 
-            print(f'Loaded {path}. Was trained for {self.n_trained_epochs} epochs')
+            try:
+                self.n_trained_epochs = checkpoint['n_trained_epochs']
+                print(f'Loaded {path}. Was trained for {self.n_trained_epochs} epochs')
+            except KeyError:
+                # in case we can't find field, just assume it was trained for all (50) epochs
+                self.n_trained_epochs = 50
+
             
         except FileNotFoundError:
             logging.info(f'No checkpoint found for "{path}" -> continue with normal training')
